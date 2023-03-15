845d1b306e9b37a4357184eba67c0fc10b6f161e
Imaeda Fuka <fuuka0503@gmail.com>
2023-03-15 13:14:20 +0900

    fuka

--- index.php
+++ index.php
@@ -9,52 +9,7 @@
 </head>
 
 <body>
-    <section>
-        <div class="header kv">
-            <div class="logo">
-                <!-- <img src="ここにロゴ" alt=""> -->
-                <div class="nav">
-                    <ul>
-                        <li>
-                            <a href="#">Menu01</a>
-                        </li>
-                        <li>
-                            <a href="#">Menu02</a>
-                        </li>
-                        <li>
-                            <a href="#">Menu03</a>
-                        </li>
-                        <li>
-                            <a href="#">Menu04</a>
-                        </li>
-                    </ul>
-                    <h2>進化し続ける「街」アメリカンビレッジマガジン</h2>
-                </div>
-            </div>
-            <div class="visu">
-                <!-- <img src="ここにロゴ2" alt=""> -->
-            </div>
-        </div>
-    </section>
-
-    <section>
-        <spann class="sub-title">Latest Articles</span>
-            <div class="card card-skin">
-                <div class="card-imgframe"></div>
-                <div class="card-textbox">
-                    <div class="card-date">
-                        date here
-                    </div>
-                    <div class="card__overviewtext">
-                        概要がはいります。概要がはいります。概要がはいります。概要がはいります。
-                        <div class="read-btn">
-                         <input type="button" value="" name="btn">
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-    </section>
+ 
 </body>
 
 </html>
