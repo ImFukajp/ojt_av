d86e09f235251998737cbae33d2668e3a9a36979
OKOMEEEE\kihik <kihika.chiiii5@gmail.com>
2023-03-20 18:25:30 +0900

    大変お待たせしました！！

--- header.php
+++ header.php
@@ -1,4 +1,42 @@
-<header>
+<!DOCTYPE html>
+<html lang="en">
 
-</header>
-<?php wp_head(); ?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/reset.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+    <?php wp_head(); ?>
+</head>
+
+<body>
+    <!-- 合体前 -->
+    <header>
+        <div class="container">
+            <!-- pc表示 -->
+            <!-- <ul class="menu">
+                <li>Menu01</li>
+                <li>Menu02</li>
+                <li>Menu03</li>
+                <li>Menu04</li>
+            </ul> -->   
+
+            <!-- sp>>>🍔 -->
+            <!-- 🍔ボタンだよ -->
+            <button type="button" class="btn js-btn">
+                <span class="btn-line"></span>
+            </button>
+
+            <!-- 🍔メニューリスト -->
+            <nav id="navi">
+                <ul class="menu">
+                    <li class="menu-list">Menu01</li>
+                    <li class="menu-list">Menu02</li>
+                    <li class="menu-list">Menu03</li>
+                    <li class="menu-list">Menu04</li>
+                </ul>
+            </nav>
+        </div>
+    </header>
---
+++ index.php
@@ -0,0 +1,47 @@
+<!-- へっだー読み込み -->
+<?php get_header(); ?>
+    <main>
+        <div class="container">
+            <!-- ここkv -->
+            <section class="kv">
+                <div class="kv-slider">
+                    <!-- js>>>kv7枚すらいだー -->
+                    <div class="img-wrap">
+                        <img src="./img/visual_1_pc.png" alt="">
+                        <!-- ★ここに他6枚もはりつける -->
+                        <!-- <img src="./img/visual_2_pc.png" alt="">
+                        <img src="./img/visual_3_pc.png" alt="">
+                        <img src="./img/visual_4_pc.png" alt="">
+                        <img src="./img/visual_5_pc.png" alt="">
+                        <img src="./img/visual_6_pc.png" alt="">
+                        <img src="./img/visual_7_pc.png" alt=""> -->
+                    </div>
+                    <!-- css>>>logo, slider, おしゃ文字 positionで調整 -->
+                    <div class="logo">
+                        <img src="./img/logo.png" alt="logo">
+                    </div>
+                    <div class="oshya-moji">
+                        <img src="./img/visual_text_pc.png" alt="American Village">
+                    </div>
+
+                </div>
+                <!-- kv下テキスト -->
+                <p>進化し続ける「街」アメリカンビレッジマガジン</p>
+            </section>
+            <!-- kvここまで -->
+
+            <!-- ふかちゃぞーん🦄 -->
+        </div>
+    </main>
+    <footer>
+        <div class="container">
+            <!-- ふかちゃぞーん🦄 -->
+        </div>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"
+        integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+    <script src="./script.js"></script>
+</body>
+
+</html>
